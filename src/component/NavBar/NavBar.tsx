fa3ca3d9b0b9beeba0efd1eaa27e3cea78ad1e84
Akin Karayun <akinkarayun1@hotmail.com>
2022-05-18 20:45:11 +0300

    changed styles of navbar

--- src/component/NavBar/NavBar.tsx
+++ src/component/NavBar/NavBar.tsx
@@ -11,7 +11,7 @@ export const NavBar: React.FC<NavBarProps> = ({ }) => {
     <nav className={css.navbar}>
 
       <Link to='/'>
-        <img width={150} height={80} alt="logo" src={'https://th.bing.com/th/id/R.014ece3e62d9fbc62796affef1636901?rik=UEJazImpV6w8kQ&pid=ImgRaw&r=0'} />
+        <img width={50} height={40} alt="logo" src={'https://th.bing.com/th/id/OIP.tCdAJl_5GiCudhsDGT2irQHaHa?pid=ImgDet&rs=1'} />
       </Link>
       <div className={css.navbarbutton}>
         <ul className={css.nav}>
